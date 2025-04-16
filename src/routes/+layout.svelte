65485f46dfdb977276b24c0107574a94c6d4f2b2
Adam Bowman <abowman@uturndata.com>
2025-04-16 16:16:20 -0500

    fix: update layout file

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,6 +2,7 @@
 	import 'bootstrap/dist/css/bootstrap.min.css';
 	import 'bootstrap-icons/font/bootstrap-icons.css';
 	import NavBar from "$lib/components/NavBar.svelte";
+	import { onMount } from 'svelte';
 	import { browser } from '$app/environment';
 	import { applyColorVariables } from '$lib/utils/colors';
 	import { authState } from '$lib/stores/auth.svelte.js';
@@ -23,6 +24,16 @@
 		if (browser) {
 			console.log('Path changed:', currentPage.url.pathname);
 			authState.updatePath(currentPage.url.pathname);
+		}
+	});
+	
+	onMount(async () => {
+		const { Auth } = await import('aws-amplify');
+		try {
+		user = await Auth.currentAuthenticatedUser();
+		console.log("✅ Authenticated:", user);
+		} catch (err) {
+		console.error("🔥 Auth error:", err);
 		}
 	});
 </script>
